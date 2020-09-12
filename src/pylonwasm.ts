dbf327fa068a44e7c8a5fa6e9c2ffc6cf1878980
y21 <btimo8822@gmail.com>
2020-09-12 14:53:32 +0000

    support more events

--- src/pylonwasm.ts
+++ src/pylonwasm.ts
@@ -1,6 +1,12 @@
 // Used to decode typed arrays (-> string)
 const decoder = new TextDecoder();
 
+type AnyCallback = (...args: any[]) => any;
+
+function createTypedNullPrototype<T, F>(obj: T): T & { [key: string]: F } {
+  return Object.setPrototypeOf(obj, null);
+}
+
 interface EventQueueItem<T = any> {
   id?: number;
   item: T;
@@ -19,6 +25,11 @@ const enum MessageOptions {
   DISABLE_USERS = 1 << 2,
   DISABLE_ALL = 1 << 3
 }
+
+const supportedEvents = [
+  discord.Event.MESSAGE_CREATE,
+  discord.Event.MESSAGE_DELETE
+];
 
 function hasFlag(all: number, bit: number) {
   return (all & bit) === bit;
@@ -59,12 +70,37 @@ class EventQueue {
   }
 }
 
+namespace Wasm {
+  // Using the `WebAssembly` namespace reports a lot of incorrect errors, so we need our own typings
+  export interface InstanceExports {
+    // Initial function that registers events and commands, returning a status code (similar to `main`, where code 0 means OK)
+    wasm_main: () => number;
+    // WebAssembly memory duh
+    memory: WebAssembly.Memory;
+    // Allocates n bytes and returns a pointer (memory address)
+    malloc: (size: number) => number;
+    // Frees memory that was previously allocated using `malloc`
+    free: (ptr: number) => void;
+    // Function that calls other C functions, e.g. command executor functions
+    call_event: (task_id: number, event_id: number, ptr: number) => void;
+  }
+
+  export interface PWInstance {
+    exports: InstanceExports;
+  }
+
+  export interface InstantiateResult {
+    module: WebAssembly.Module;
+    instance: PWInstance;
+  }
+}
+
 export class PylonWasm {
-  public wasm: WebAssembly.WebAssemblyInstantiatedSource | undefined;
+  public wasm: Wasm.InstantiateResult | undefined;
   public wasmSource: string | undefined;
 
-  private dv: DataView | undefined;
-  private eventQueue: EventQueue;
+  protected dv: DataView | undefined;
+  protected eventQueue: EventQueue;
 
   /**
    * Creates a new PylonWasm object
@@ -83,46 +119,21 @@ export class PylonWasm {
   }
 
   /**
+   * @deprecated
    * Pass this function to `discord.on(discord.Event.MESSAGE_CREATE)` to listen for
    * MESSAGE_CREATE events in C code
+   * Note: this function is deprecated, you should use `.forEvent(discord.on.MESSAGE_CREATE)` instead
    */
   public async messageCreate(message: discord.Message) {
-    if (!message.content || message.content.length === 0) return;
+    return events.messageCreate.call(this, message);
+  }
 
-    if (!this.wasm) {
-      if (!this.wasmSource) {
-        throw new Error(
-          'Cannot load WebAssembly binary (`wasmSource` is undefined)'
-        );
-      }
-
-      await fetch(this.wasmSource)
-        .then((x) => x.arrayBuffer())
-        .then((x) => this.__load_wasm__(x));
+  public forEvent(event: discord.Event) {
+    if (!supportedEvents.includes(event) || !eventTable[event]) {
+      throw new Error('unsupported event: ' + event);
     }
 
-    const taskId = this.eventQueue.next({
-      item: message
-    });
-
-    const ptr = this.__malloc_str__(message.content);
-
-    if (ptr === 0) {
-      throw new Error('Memory allocation failed');
-    }
-
-    try {
-      this.wasm!.instance.exports.call_message_create(
-        taskId,
-        ptr,
-        message.content.length
-      );
-    } catch (e) {
-      console.error('[pylonwasm runtime error]', e);
-    } finally {
-      this.wasm!.instance.exports.free(ptr);
-      this.eventQueue.done(taskId);
-    }
+    return (<AnyCallback>eventTable[event]).bind(this);
   }
 
   // ===============
@@ -141,7 +152,7 @@ export class PylonWasm {
    * Loads a WebAssembly binary
    * This function also attempts to call `wasm_main`.
    */
-  private async __load_wasm__(source: ArrayBuffer) {
+  protected async __load_wasm__(source: ArrayBuffer) {
     // @ts-ignore
     this.wasm = await WebAssembly.instantiate(source, {
       env: {
@@ -173,7 +184,7 @@ export class PylonWasm {
    * Gets a value by its pointer (address in wasm memory).
    */
 
-  private __get_ptr__(
+  protected __get_ptr__(
     ptr: number,
     len: number,
     stringify = false
@@ -191,7 +202,7 @@ export class PylonWasm {
   /**
    * Sets a value at a given memory address.
    */
-  private __set_ptr__(ptr: number, val: any): boolean {
+  protected __set_ptr__(ptr: number, val: any): boolean {
     if (!this.dv) return false;
 
     switch (val.constructor) {
@@ -216,7 +227,7 @@ export class PylonWasm {
    * Allocates heap memory for a string, sets the value in memory and returns the address, expecting the caller to free memory(!)
    * Returns 0 if `wasm` is not yet loaded, or if memory allocation failed.
    */
-  private __malloc_str__(value: string): number {
+  protected __malloc_str__(value: string): number {
     if (!this.wasm) return 0;
     const ptr = this.wasm.instance.exports.malloc(
       value.length * IntSize.UINT8 + 1
@@ -231,14 +242,15 @@ export class PylonWasm {
    * [External function]
    * This function is called for sending messages in C code, receiving the pointer of message content and its length.
    */
-  private async __wasm_send_message__(
+  protected async __wasm_send_message__(
     task_id: number,
     flags: number,
     ct_ptr: number,
     ct_len: number
   ) {
     const event = this.eventQueue.get<discord.Message>(task_id);
-    if (!event) return this.__handle_wasm_error__(new Error('Task not found.'));
+    if (!event || !event.item)
+      return this.__handle_wasm_error__(new Error('Task not found.'));
 
     const content = <string | null>this.__get_ptr__(ct_ptr, ct_len, true);
     if (content === null)
@@ -259,7 +271,7 @@ export class PylonWasm {
    * [External function]
    * This function is called to log a string to the console. This allows you to use `console.log` in C code.
    */
-  private __wasm_log__(log_ptr: number, log_len: number, raw: number) {
+  protected __wasm_log__(log_ptr: number, log_len: number, raw: number) {
     console.log(this.__get_ptr__(log_ptr, log_len, raw !== 0));
   }
 
@@ -267,7 +279,7 @@ export class PylonWasm {
    * Throwing exceptions doesn't really work well right now, so we just log the error to the console
    * This will probably be changed to actually throw exceptions that can be caught in some way at some point
    */
-  private __handle_wasm_error__(e: Error, task_id?: number) {
+  protected __handle_wasm_error__(e: Error, task_id?: number) {
     console.error(`[pylonpp${task_id ? `-${task_id}` : ''}]`, e);
   }
 }
@@ -279,3 +291,69 @@ function __set_ptr_str__(dv: DataView, ptr: number, val: string) {
 
   dv.setUint8(ptr + val.length, 0); // null terminate c-string
 }
+
+namespace events {
+  async function run(this: PylonWasm, event: discord.Event, itemData: any) {
+    if (!this.wasm) {
+      if (!this.wasmSource) {
+        throw new Error(
+          'Cannot load WebAssembly binary (`wasmSource` is undefined)'
+        );
+      }
+
+      await fetch(this.wasmSource)
+        .then((x) => x.arrayBuffer())
+        .then(this.__load_wasm__.bind(this));
+    }
+
+    const taskId = this.eventQueue.next({ item: itemData.item });
+
+    const ptr = this.__malloc_str__(itemData.malloc);
+
+    if (ptr === 0) {
+      throw new Error('Memory allocation failed');
+    }
+
+    try {
+      this.wasm!.instance.exports.call_event(
+        taskId,
+        supportedEvents.indexOf(event),
+        ptr
+      );
+    } catch (e) {
+      console.error('[pylonwasm runtime error]', e);
+    } finally {
+      this.wasm!.instance.exports.free(ptr);
+      this.eventQueue.done(taskId);
+    }
+  }
+
+  export async function messageCreate(
+    this: PylonWasm,
+    message: discord.Message
+  ) {
+    if (!message.content || message.content.length === 0) return;
+
+    return run.call(this, discord.Event.MESSAGE_CREATE, {
+      item: message,
+      malloc: message.content
+    });
+  }
+
+  export async function messageDelete(
+    this: PylonWasm,
+    message: discord.Message
+  ) {
+    if (!message.content || message.content.length === 0) return;
+
+    return run.call(this, discord.Event.MESSAGE_DELETE, {
+      item: message,
+      malloc: message.content
+    });
+  }
+}
+
+const eventTable = createTypedNullPrototype({
+  [discord.Event.MESSAGE_CREATE]: events.messageCreate,
+  [discord.Event.MESSAGE_DELETE]: events.messageDelete
+});
